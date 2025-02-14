75432c92f6f2e09cf9a2f856a07667ca82a6baac
pradhyum1998 <pradhyumpatil@gmail.com>
2025-02-14 16:24:16 +0530

    changes done in Clone-Branch

--- FirstSQL.sql
+++ FirstSQL.sql
@@ -4,3 +4,7 @@ select * from Emp
 
 insert into Emp (id,Name,city) VALUES(1,'Abhishake','Hyd'),(2,'Milind','Hyd'),(3,'Viraj','Bom'),(4,'Karan','Bom'),(5,'Himanshu','Nashik')
 ,(6,'Darshan','Nashik')
+
+select * from Emp where city = 'Nashik'
+
+select * from Emp where city = 'hydrabad'
