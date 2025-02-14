f8115c7cd4a19598bdd9e116458b242d94d6314b
pradhyum1998 <pradhyumpatil@gmail.com>
2025-02-14 11:36:51 +0530

    +

---
+++ FirstSQL.sql
@@ -0,0 +1,6 @@
+--create table Emp (id int, Name varchar(25),city varchar(25))
+
+select * from Emp
+
+insert into Emp (id,Name,city) VALUES(1,'Abhishake','Hyd'),(2,'Milind','Hyd'),(3,'Viraj','Bom'),(4,'Karan','Bom'),(5,'Himanshu','Nashik')
+,(6,'Darshan','Nashik')
